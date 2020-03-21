8f32042b4efa7b65f28406b3e186a4412f3e1751
Andre Landgraf <andre.timo.landgraf@gmail.com>
2020-03-21 15:44:42 -0700

    added offline support

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,5 +15,31 @@ module.exports = {
         },
       },
     },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'Andre Landgraf',
+        short_name: 'andrelandgraf',
+        start_url: '/',
+        background_color: '#fff',
+        theme_color: '#1890ff',
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: 'standalone',
+        icon: 'src/assets/img/appicon.jpg', // This path is relative to the root of the site.
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
+        crossOrigin: 'use-credentials',
+      },
+    },
+    // You can add a manifest file. Ensure that the manifest plugin is listed before
+    // the offline plugin so that the offline plugin can cache the created manifest.webmanifest.
+    {
+      resolve: 'gatsby-plugin-offline',
+      options: {
+        precachePages: ['/', '/uses'],
+      },
+    },
   ],
 };
