6eca30306970366b19fa15f353325a3b1ef4ac04
Andre Landgraf <andre.timo.landgraf@gmail.com>
2023-01-08 18:28:49 -0800

    new image

--- app/utilities/images.ts
+++ app/utilities/images.ts
@@ -1,6 +1,6 @@
 export const images = {
   resumeImage: {
-    src: 'https://res.cloudinary.com/andre-landgraf/image/upload/c_scale,f_auto,q_auto,w_350/v1637689043/portrait.jpg',
+    src: 'https://res.cloudinary.com/andre-landgraf/image/upload/c_scale,f_auto,q_auto,w_350/v1673231287/andrelandgraf.dev/profile_u3k4ym.jpg',
     alt: 'Andre Landgraf smiling into the camera',
   },
 };
