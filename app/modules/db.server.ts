b4a5ee86978bdc485bc21aac43b0bcd5a2bc60e8
Andre Landgraf <andre.timo.landgraf@gmail.com>
2023-11-05 22:19:03 -0800

    add neon connection pooiling

--- app/modules/db.server.ts
+++ app/modules/db.server.ts
@@ -1,4 +1,10 @@
+// Import needed packages
+import { neonConfig, Pool } from '@neondatabase/serverless';
+import { PrismaNeon } from '@prisma/adapter-neon';
 import { PrismaClient } from '@prisma/client';
+import ws from 'ws';
+
+import { getPrivateEnvVars } from './env.server';
 
 let db: PrismaClient;
 
@@ -7,14 +13,23 @@ declare global {
   var __db: PrismaClient | undefined;
 }
 
+function buildPrismaClient() {
+  const { databaseUrl } = getPrivateEnvVars();
+  neonConfig.webSocketConstructor = ws;
+
+  const pool = new Pool({ connectionString: databaseUrl });
+  const adapter = new PrismaNeon(pool);
+  return new PrismaClient({ adapter });
+}
+
 // this is needed because in development we don't want to restart
 // the server with every change, but we want to make sure we don't
 // create a new connection to the DB with every change either.
 if (process.env.NODE_ENV === 'production') {
-  db = new PrismaClient();
+  db = buildPrismaClient();
 } else {
   if (!global.__db) {
-    global.__db = new PrismaClient();
+    global.__db = buildPrismaClient();
   }
   db = global.__db;
 }
--- app/modules/env.server.ts
+++ app/modules/env.server.ts
@@ -1,7 +1,11 @@
+import dotenv from 'dotenv';
 import invariant from 'tiny-invariant';
+
+dotenv.config();
 
 type PrivateEnvVars = {
   readContentFrom: 'locale' | 'github' | 'production';
+  databaseUrl: string;
   githubAccessToken: string;
   githubRepoAPIUrl: string;
   openAIKey: string;
@@ -11,12 +15,15 @@ const DEV_FETCH_FROM = 'production';
 
 export function getPrivateEnvVars(): PrivateEnvVars {
   const readContentFrom = process.env.NODE_ENV === 'production' ? 'production' : DEV_FETCH_FROM;
+  const databaseUrl = process.env.DATABASE_URL;
+  invariant(databaseUrl && typeof databaseUrl === 'string', 'Database URL is not defined');
   const githubAccessToken = process.env.GITHUB_ACCESS_TOKEN;
   const githubRepoAPIUrl = 'https://api.github.com/repos/andrelandgraf/andrelandgraf/contents';
   invariant(githubAccessToken && typeof githubAccessToken === 'string', 'Github access token is not defined');
   const openAIKey = process.env.OPEN_AI_KEY;
   invariant(openAIKey && typeof openAIKey === 'string', 'Open AI key is not defined');
   return {
+    databaseUrl,
     githubAccessToken,
     githubRepoAPIUrl,
     readContentFrom,
