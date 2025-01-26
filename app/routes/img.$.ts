0f7e59b87e84dfee489dbedc48bd2fedc3bdac1e
andrelandgraf <andre.timo.landgraf@gmail.com>
2025-01-26 13:10:32 -0800

    use openimg-node

--- app/routes/img.$.ts
+++ app/routes/img.$.ts
@@ -1,20 +1,16 @@
-import { createReadableStreamFromReadable, LoaderFunctionArgs } from '@remix-run/node';
-import fs from 'node:fs';
-import fsp from 'node:fs/promises';
-import path from 'node:path';
-import sharp from 'sharp';
-import nodeFetch from 'node-fetch';
+import { LoaderFunctionArgs } from '@remix-run/node';
 import { env } from '~/modules/env.server.ts';
 import { internalServerError, notFound } from '~/modules/responses.server.ts';
 import { captureException } from '~/modules/sentry/capture.server.ts';
 import { type ObjectFit } from '~/modules/image-opt/utils.ts';
 import { getServerTiming } from '~/modules/server-timing.server.ts';
+import { getImgResponse } from 'openimg-node';
 
 export { headers } from '~/modules/headers.server.ts';
 
-function getIntOrNull(value: string | null) {
+function getIntOrUndefined(value: string | null) {
   if (value === null) {
-    return null;
+    return undefined;
   }
 
   return Number.parseInt(value);
@@ -33,117 +29,77 @@ type ImageType = 'public' | 'gen' | 'pocketbase';
 function getFilePath(
   type: ImageType,
   fileId: string,
-  width: number | null,
-  height: number | null,
-  fit: 'cover' | 'contain',
+  width: number | string,
+  height: number | string,
+  fit: 'cover' | 'contain' | 'base',
 ) {
-  const widthInfo = `w${width || '0'}`;
-  const heightInfo = `h${height || '0'}`;
-  const fileName = fileId.replace(/\/./g, '-');
-  return `${env.server.volumePath}/images/${type}/v1-${fileName}-${widthInfo}-${heightInfo}-${fit}.webp`;
+  const widthInfo = `w-${width}`;
+  const heightInfo = `h-${height}`;
+  const fileName = fileId.replaceAll('.', '-');
+  return `${env.server.volumePath}/images/${type}/${fileName}-${widthInfo}-${heightInfo}-${fit}.webp`;
 }
 
-/**
- * Inspired by Jacob Ebey's gist: https://gist.github.com/jacob-ebey/3a37a86307de9ef22f47aae2e593b56f
- */
 export async function loader({ request }: LoaderFunctionArgs) {
-  const { time, getHeaderField, getServerTimingHeader } = getServerTiming();
-
+  const { getHeaderField, getServerTimingHeader } = getServerTiming();
   const url = new URL(request.url);
-  const width = getIntOrNull(url.searchParams.get('w'));
-  const height = getIntOrNull(url.searchParams.get('h'));
-  const fit = getObjectFit(url.searchParams.get('fit'));
-  if (Number.isNaN(width) || Number.isNaN(height)) {
-    return new Response('w and h query parameters must be numbers', {
-      status: 400,
-      statusText: 'Bad Request',
-    });
-  }
 
-  let filePath: string | null = null;
-  let originUrl: string | null = null;
-
-  // path: /img/public/x/y/z.png for images in public folder
-  if (url.pathname.startsWith('/img/public/')) {
-    const urlPath = url.pathname;
-    const publicFileId = urlPath.replace('/img/public/', '');
-    filePath = getFilePath('public', publicFileId, width, height, fit);
-    originUrl = `${env.server.origin}/${publicFileId}`;
-  }
-
-  // path: /img/gen/x/y/z.png for images generated via modules/image-gen
-  if (url.pathname.startsWith('/img/gen/')) {
-    const urlPath = url.pathname;
-    const genFileId = urlPath.replace('/img/gen/', '');
-    filePath = getFilePath('gen', genFileId, width, height, fit);
-    originUrl = `${env.server.origin}/${genFileId}`;
-  }
-
-  if (!filePath || !originUrl) {
-    return notFound();
-  }
-
-  if (fs.existsSync(filePath)) {
-    const readable = fs.createReadStream(filePath);
-    return new Response(createReadableStreamFromReadable(readable), {
-      headers: {
-        'Content-Type': 'image/webp',
-        'Cache-Control': `public, max-age=${60 * 60 * 24}`,
-        'Server-Timing': getHeaderField(),
-      },
-    });
-  }
-
-  console.log('Cache miss, fetching image from origin:', originUrl);
-  const res = await time('fetchImg', () => nodeFetch(`${originUrl}?${url.searchParams.toString()}`, {}));
-  const resBody = res.body;
-  if (!res.ok || !resBody) {
-    captureException(new Error(`Failed to fetch image from origin: ${originUrl}`));
-    return internalServerError(getServerTimingHeader());
-  }
-
-  const sharpInstance = sharp();
-  sharpInstance.on('error', (error) => {
-    console.error(error);
-    captureException(error);
-  });
-  if (width || height) {
-    sharpInstance.resize(width, height, { fit });
-  }
-  sharpInstance.webp({ effort: 6 });
+  const headers = new Headers();
+  headers.set('Content-Type', 'image/webp');
+  headers.set('Cache-Control', `public, max-age=${60 * 60 * 24}`); // 24h
+  headers.set('Server-Timing', getHeaderField());
 
   try {
-    await time(
-      'mkdirRec',
-      fsp.mkdir(path.dirname(filePath), { recursive: true }).catch(() => {}),
-    );
-    const transformStream = resBody.pipe(sharpInstance);
-    const cacheFileStream = fs.createWriteStream(filePath);
-    await time(
-      'transformImage',
-      new Promise<void>((resolve, reject) => {
-        transformStream.pipe(cacheFileStream);
-        transformStream.on('end', () => {
-          resolve();
-        });
-        transformStream.on('error', async (error: Error) => {
-          await fsp.rm(filePath).catch(() => {});
-          reject(error);
-        });
-      }),
-    );
+    return getImgResponse(request, headers, {
+      getImgParams: () => {
+        const width = getIntOrUndefined(url.searchParams.get('w'));
+        const height = getIntOrUndefined(url.searchParams.get('h'));
+        if (Number.isNaN(width) || Number.isNaN(height)) {
+          return new Response('w and h query parameters must be numbers', {
+            status: 400,
+            statusText: 'Bad Request',
+          });
+        }
 
-    const readable = fs.createReadStream(filePath);
-    return new Response(createReadableStreamFromReadable(readable), {
-      headers: {
-        'Content-Type': 'image/webp',
-        'Cache-Control': `public, max-age=${60 * 60 * 24}`, // cache img 24 hours in browser
-        'Server-Timing': getHeaderField(),
+        let fit = getObjectFit(url.searchParams.get('fit'));
+
+        return {
+          width,
+          height,
+          fit,
+          targetFormat: 'webp',
+        };
+      },
+      getImgSources: (_, { width, height, fit }) => {
+        let cacheSrc: string | null = null;
+        let originSrc: string | null = null;
+
+        // path: /img/public/x/y/z.png for images in public folder
+        if (url.pathname.startsWith('/img/public/')) {
+          const urlPath = url.pathname;
+          originSrc = "." + urlPath.replace('/img', ''); // "./public/x/y/z.png"
+          const publicFileId = urlPath.replace('/img/public/', ''); // "x/y/z.png"
+          cacheSrc = getFilePath('public', publicFileId, width || 'base', height || 'base', fit || 'base');
+          console.log("stats", cacheSrc, originSrc);
+        }
+
+        // path: /img/gen/x/y/z.png for images generated via modules/image-gen
+        if (url.pathname.startsWith('/img/gen/')) {
+          const urlPath = url.pathname;
+          const genFileId = urlPath.replace('/img/gen/', ''); // "x/y/z.png"
+          cacheSrc = getFilePath('gen', genFileId, width || 'base', height || 'base', fit || 'base');
+          originSrc = `${env.server.origin}/${genFileId}`; // URL to call generate endpoint
+        }
+
+        if (!cacheSrc || !originSrc) {
+          return notFound();
+        }
+
+        return { cacheSrc, originSrc };
       },
     });
-  } catch (error) {
-    console.error(error);
-    captureException(error);
+  } catch (err: unknown) {
+    console.error(err);
+    captureException(err);
     return internalServerError(getServerTimingHeader());
   }
 }
